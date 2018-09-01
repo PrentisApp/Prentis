8154437e33fa639975cb1fe00ee4502c729b84db
Shakeeb Majid <shakeebmajid@ufl.edu>
2018-09-01 17:14:46 -0400

    Basic Project Environment

---
+++ Prentis/Prentis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prentis
+//
+//  Created by Shakeeb Majid on 9/1/18.
+//  Copyright © 2018 PrentisApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
