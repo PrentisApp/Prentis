4fa7c57dfda3fafaa1e0242d69f53c9f50d575b6
Shakeeb Majid <shakeebmajid@ufl.edu>
2018-09-30 21:14:58 -0400

    mute audio, end call, switch cameras, and draggable local video view features added

--- Prentis/Prentis/ViewController.swift
+++ Prentis/Prentis/ViewController.swift
@@ -18,7 +18,10 @@ class ViewController: UIViewController, AgoraRtcEngineDelegate {
     @IBOutlet weak var localVideoMutedBg: UIImageView!
     @IBOutlet weak var localVideoMutedIndicator: UIImageView!
     
+    @IBOutlet weak var endButton: UIButton!
     var agoraKit: AgoraRtcEngineKit?
+    var workItem: DispatchWorkItem?
+    var localCenter = CGPoint(x: 0,y :0)
     
     func initializeAgoraEngine() {
         agoraKit = AgoraRtcEngineKit.sharedEngine(withAppId: "be579b93d395458bb85fae03f92ad582", delegate: self)
@@ -60,21 +63,48 @@ class ViewController: UIViewController, AgoraRtcEngineDelegate {
         }
     }
     
+    
+    
     func leaveChannel() {
         agoraKit?.leaveChannel(nil)
+        hideControlButtons()
+        UIApplication.shared.isIdleTimerDisabled = false
+        remoteVideo.removeFromSuperview()
+        localVideo.removeFromSuperview()
+        agoraKit = nil
     }
     
+    func hideControlButtons() {
+        controlButtons.isHidden = true
+    }
+    
+    @objc func toggleControlButtons(_ sender: UITapGestureRecognizer) {
+        if controlButtons.isHidden {
+            controlButtons.isHidden = false
+            workItem = DispatchWorkItem{ self.hideControlButtons() }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5.0, execute: workItem!)
+        } else {
+            controlButtons.isHidden = true
+            workItem?.cancel()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0, execute: {
+            self.hideControlButtons()
+        })
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.toggleControlButtons(_:)))
+        remoteVideo.addGestureRecognizer(tap)
+        endButton.layer.zPosition = 2
         initializeAgoraEngine()
-        
+        controlButtons?.backgroundColor = UIColor(white: 1, alpha: 0.0)
         setChannelProfile()
         
         enableVideo()
         
+        localVideo.layer.cornerRadius = 15;
+        localVideo.layer.masksToBounds = true;
         let configuration = AgoraVideoEncoderConfiguration(size:
             AgoraVideoDimension640x360, frameRate: .fps15, bitrate: 400,
                                         orientationMode: .fixedPortrait)
@@ -87,8 +117,35 @@ class ViewController: UIViewController, AgoraRtcEngineDelegate {
         
         setupRemoteVideo()
         
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(self.panLocal(_:)))
+        localVideo.addGestureRecognizer(pan)
     }
+    
+    @objc func panLocal(_ pan: UIPanGestureRecognizer) {
+        if pan.state == .began {
+            localCenter = localVideo.center // store old button center
+        } else if pan.state == .ended || pan.state == .failed || pan.state == .cancelled {
+            let location = pan.location(in: view)
+            localVideo.center = location // restore button center
+        } else {
+            let location = pan.location(in: view) // get pan location
+            localVideo.center = location // set button to where finger is
+        }
+    }
+    
 
+    @IBAction func onMute(_ sender: UIButton ) {
+        sender.isSelected = !sender.isSelected
+        agoraKit?.muteLocalAudioStream(sender.isSelected)
+    }
+    @IBAction func onEndCall(_ sender: Any) {
+        leaveChannel()
+    }
+    
+    @IBAction func onSwitchCamera(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        agoraKit?.switchCamera()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
