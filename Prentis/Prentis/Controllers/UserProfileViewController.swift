480c4015cc5638557ae80421710bc4a9be513c9b
kasistores <kasistores@gmail.com>
2018-11-21 17:48:39 -0500

    custom segmented Control

--- Prentis/Prentis/Controllers/UserProfileViewController.swift
+++ Prentis/Prentis/Controllers/UserProfileViewController.swift
@@ -11,12 +11,12 @@ import Firebase
 import FirebaseAuth
 import FirebaseFirestore
 import FirebaseStorage
+import MXSegmentedControl
 
 class UserProfileViewController: UIViewController {
-
-    @IBOutlet weak var interestsView: UIView!
     
-    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var segmentedControl1: MXSegmentedControl!
+    @IBOutlet weak var interestsView: UIView!
     @IBOutlet weak var profileView: UIView!
     
     @IBOutlet weak var expertiseView: UIView!
@@ -26,28 +26,55 @@ class UserProfileViewController: UIViewController {
         super.viewDidLoad()
         print("You've arrived bih")
         // Do any additional setup after loading the view.
+        segmentedControl1.append(title: "Profile")
+        segmentedControl1.append(title: "Interests")
+        segmentedControl1.append(title: "Expertise")
+        segmentedControl1.indicator.linePosition = .top
+        segmentedControl1.addTarget(self, action: #selector(changeIndex(segmentedControl:)), for: .valueChanged)
+    }
+    
+    @objc func changeIndex(segmentedControl: MXSegmentedControl) {
+        
+        if let segment = segmentedControl.segment(at: segmentedControl.selectedIndex) {
+            
+            if segment.titleLabel?.text! == "Profile"{
+                self.profileView.isHidden = false
+                self.interestsView.isHidden = true
+                self.expertiseView.isHidden = true
+            }
+            else if segment.titleLabel?.text! == "Interests"{
+                self.profileView.isHidden = true
+                self.interestsView.isHidden = false
+                self.expertiseView.isHidden = true
+            }
+            else{
+                self.profileView.isHidden = true
+                self.interestsView.isHidden = true
+                self.expertiseView.isHidden = false
+            }
+        }
     }
 
-    @IBAction func chooseView(_ sender: UISegmentedControl) {
-        if sender.selectedSegmentIndex == 0{
-            self.profileView.isHidden = false
-            self.interestsView.isHidden = true
-            self.expertiseView.isHidden = true
-            self.segmentControl.tintColor = self.profileView.tintColor
-        }
-        else if sender.selectedSegmentIndex == 1{
-            self.profileView.isHidden = true
-            self.interestsView.isHidden = false
-            self.expertiseView.isHidden = true
-            self.segmentControl.tintColor = self.interestsView.tintColor
-        }
-        else{
-            self.profileView.isHidden = true
-            self.interestsView.isHidden = true
-            self.expertiseView.isHidden = false
-            self.segmentControl.tintColor = self.expertiseView.tintColor
-        }
-    }
+//    @IBAction func chooseView(_ sender: UISegmentedControl) {
+//        if sender.selectedSegmentIndex == 0{
+//            self.profileView.isHidden = false
+//            self.interestsView.isHidden = true
+//            self.expertiseView.isHidden = true
+//            self.segmentControl.tintColor = self.profileView.tintColor
+//        }
+//        else if sender.selectedSegmentIndex == 1{
+//            self.profileView.isHidden = true
+//            self.interestsView.isHidden = false
+//            self.expertiseView.isHidden = true
+//            self.segmentControl.tintColor = self.interestsView.tintColor
+//        }
+//        else{
+//            self.profileView.isHidden = true
+//            self.interestsView.isHidden = true
+//            self.expertiseView.isHidden = false
+//            self.segmentControl.tintColor = self.expertiseView.tintColor
+//        }
+//    }
     /*
      // MARK: - Navigation
      
